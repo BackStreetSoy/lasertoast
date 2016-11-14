ea60b4d2166bfdbe52986feeba676b10947fb369
BackStreetSoy <jordanedwardobey@gmail.com>
2016-11-14 20:29:45 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-beaks = Comic.create(title: "Beaks", image: "/assets/beaks.png") 
+beaks = Comic.create(title: "Beaks", image: "beaks.png") 

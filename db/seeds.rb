6ed892db0a81344e56f883dd14c98ad054ebe18b
BackStreetSoy <jordanedwardobey@gmail.com>
2017-04-25 16:15:17 +0200

    creating new_layout, added new content, and new buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-beaks = Comic.create(title: "Beaks", image: "beaks.png")
-cyclops = Comic.create(title: "Cyclops", image: "cyclops.png")
-truestform = Comic.create(title: "danstruestform", image:"danstruestform.png")
-heads = Comic.create(title: "heads", image: "heads.png")
-denial_burger = Comic.create(title:"denial_burger", image: "denial_burger.png")
-the_diner = Comic.create(title: "the_diner", image: "the_diner.png")
-what = Comic.create(title: "what", image:"what.png")
-anarchist_toast = Comic.create(title: "anarchist_toast", image: "anarchist_toast.png")
-kkk = Comic.create(title: "kkk", image: "bandg.png")
-magic = Comic.create(title:"magic", image:"magic.png")
-shitler = Comic.create(title: "shitlazer", image: "shitlerkitty.png")
-what = Comic.create(title: "What", image: "what.png")
-kitler = Comic.create(title: "kitler", image: "kitler.png")
-outie = Comic.create(title: "outie", image: "outie1.png")
-oops = Comic.create(title: "oops", image: "oops.png")
+toast_messiah = Comic.create(title: "toast_messiah", image: "toastworship.png");
+page_2 = Comic.create(title: "page_2", image:"page2.png");
+
 
 
